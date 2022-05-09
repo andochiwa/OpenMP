687924513a4bf1cd573d42e2ed9d580140792a78
andochiwa <a1066079469@gmail.com>
2022-05-10 00:04:31 +0900

    multi matrix parallel

--- hw/multi_matrix_parallel.cpp
+++ hw/multi_matrix_parallel.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <vector>
 #include <random>
 #include <omp.h>
@@ -7,27 +8,48 @@ using namespace std;
 
 void write_to_file(int& thread_size, vector<double>& counts) {
     ofstream ofs;
-    ofs.open("data.txt", ios::out);
+    string file_name = "data" + to_string(thread_size) + ".txt";
+    ofs.open(file_name, ios::out);
     ofs << "thread_size: " << thread_size << ", ";
     for (auto& c : counts) {
         ofs << c << " ";
     }
-    ofs << endl << "average: " << accumulate(counts.begin(), counts.end(), 0.0) / counts.size();
+    ofs << endl << "average: " << accumulate(counts.begin(), counts.end(), 0.0) / (double) counts.size();
     ofs.close();
+}
+
+template<typename T>
+double gemm(const vector<vector<T>>& a, const vector<vector<T>>& b, vector<vector<T>>& c, int size) {
+    double start, end;
+    start = omp_get_wtime();
+    #pragma omp parallel for shared(a, b, c, size)
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            T temp = 0;
+            for (int k = 0; k < size; k++) {
+                temp += a[i][k] * b[k][j];
+            }
+            c[i][j] = temp;
+        }
+    }
+    end = omp_get_wtime();
+    cout << "execution time = " << end - start << endl;
+    return end - start;
 }
 
 int main() {
     // initial
-    int size = 2000;
-    int thread_size = 16;
+    int size = 800;
+    int thread_size = 1;
+    cout << "please enter the threads size:";
+    cin >> thread_size;
     vector<vector<int>> a(size, vector<int> (size)), b(size, vector<int> (size));
-    vector<vector<int>> c(size, vector<int> (size, 0));
+    vector<vector<int>> c(size, vector<int> (size, 0)), d(size, vector<int> (size, 0));
     vector<double> counts;
-    uniform_int_distribution<int> u(INT_MIN, INT_MAX);
+    uniform_int_distribution<int> u(INT_MIN / 2, INT_MAX / 2);
     default_random_engine e;
     omp_set_num_threads(thread_size);
 
-    // computing
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
             a[i][j] = u(e);
@@ -35,21 +57,10 @@ int main() {
         }
     }
 
-    double start, end;
-
-    for (int freq = 0; freq < 10; freq++) {
-        start = omp_get_wtime();
-        #pragma omp parallel for shared(a, b, c, size)
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                for (int k = 0; k < size; k++) {
-                    c[i][j] += a[i][k] * b[k][j];
-                }
-            }
-        }
-        end = omp_get_wtime();
-        printf("execution time = %f\n", end - start);
-        counts.push_back(end - start);
+    // computing
+    for (int freq = 0; freq < 5; freq++) {
+        double time = gemm(a, b, c, size);
+        counts.push_back(time);
     }
 
     write_to_file(thread_size, counts);
